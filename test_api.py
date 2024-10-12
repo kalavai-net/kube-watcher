1f45563e2be2078cf2b765977d38b73ca658443d
Carlos Fernandez Musoles <carlos@kalavai.net>
2024-10-12 17:30:39 +0100

    bumped version

--- test_api.py
+++ test_api.py
@@ -4,69 +4,14 @@ import datetime
 import time
 
 
-URL = "http://0.0.0.0:8000"
+URL = "http://127.0.0.1:8000"
 
 if __name__ == "__main__":
     
-    params = dict(
-        namespace="default"
-    )
     
-    response = requests.post(
-        f"{URL}/v1/list_deepsparse_deployments",
-        json=params
+    response = requests.get(
+        f"{URL}/v1/get_nodes",
+        headers={"X-API-KEY": "r"}
     )
     print(json.dumps(response.json(), indent=3))
-    exit()
-    # print(json.dumps(response.json(), indent=3))
-    
-    # exit()
-    
-    # now_time = datetime.date(2023, 12, 31)
-    # start = (now_time - datetime.timedelta(days=13)).strftime('%Y-%m-%dT%H:%M:%SZ')
-    # now = now_time.strftime('%Y-%m-%dT%H:%M:%SZ')
-    # formatted_window = f"{start},{now}"
-    # print(formatted_window)
-    # params = dict(
-    #     namespace_names=["kube-system"],
-    #     kubecost_params={
-    #         "window": formatted_window
-    #     }
-    # )
-    # response = requests.post(
-    #     f"{URL}/v1/get_namespaces_cost",
-    #     json=params
-    # )
-    # print(json.dumps(response.json(), indent=3))
-    
-    params = dict(
-        deployment_name="mpt-7b",
-        namespace="carlosfm",
-        deepsparse_model_id="zoo:mpt-7b-dolly_mpt_pretrain-pruned50_quantized",
-        ephemeral_memory="42Gi",
-        ram_memory="12Gi",
-        num_cores=4,
-        task="chat",
-        replicas=1
-    )
-    response = requests.post(
-        f"{URL}/v1/deploy_deepsparse_model",
-        json=params
-    )
-    print(json.dumps(response.json(), indent=3))
-    
-    time.sleep(15)
-    
-    params = dict(
-        deployment_name="my-sparse-model",
-        namespace="carlosfm"
-    )
-    response = requests.post(
-        f"{URL}/v1/delete_deepsparse_model",
-        json=params
-    )
-    print(json.dumps(response.json(), indent=3))
-    
-    
-    
     
